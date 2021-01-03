298f6767afd23e88d93962c9752c0c2165d9afb7
Divana13 <dintccd@gmail.com>
2021-01-03 22:17:06 +0700

    12

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const authController = require('../controller/authController');
 
 const router = Router();
 
-router.get('/singup', authController.signupGet);
+router.get('/signup', authController.signupGet);
 router.post('/singup', authController.signupPost);
 router.get('/login', authController.loginGet);
 router.post('/login', authController.loginPost);
